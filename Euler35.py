8f6bded2eb3e4817539201b1e9b0e5bec6aaf1ec
ThomasR75 <ThomasR75@gmail.com>
2019-10-12 19:21:12 +0900

    first try

--- Euler35.py
+++ Euler35.py
@@ -1,6 +1,6 @@
 # Euler 35 Number of Circular Primes < 1mio.
 # Create Primes below 1mio
-primemax = 102
+primemax = 1000000
 marked = [0] * primemax
 primes = [2, ]
 value = 3
@@ -21,34 +21,21 @@ print(a)
 item = 0
 circularlist = []
 while item < a:
-    stumble = 0
     z = 0
     x = 0
     number = primes[item]
-    numberstring = str(number)
-    newnumberstring = str(number)
-    numbercounter = len(newnumberstring)
+    numberstr = str(number)
+    numbercounter = len(numberstr)
+    rotationlist = []
     while z < numbercounter:
-        numberstring1 = newnumberstring[0:1]
-        numberstring2 = newnumberstring[1:]
-        newnumberstring = numberstring2+numberstring1
-        print(numbercounter)
-        if int(newnumberstring) not in primes:
-            stumble += 1
-            print (stumble)
+        if int(numberstr) in primes:
+            rotationlist.append(numberstr)
+        numberstr = str(numberstr[1:] + numberstr[0])
         z += 1
-        if stumble < 1:
-            newnewnumberstring = str(number)
-            newnumbercounter = len(newnewnumberstring)
-            while x < newnumbercounter:
-                numberstring1 = newnewnumberstring[0:1]
-                numberstring2 = newnewnumberstring[1:]
-                if int(newnewnumberstring) not in circularlist:
-                    circularlist.append(int(newnewnumberstring))
-                    print (stumble)
-                newnewnumberstring = numberstring2 + numberstring1
-
-                x += 1
-
+    if len(rotationlist) == numbercounter:
+        for j in rotationlist:
+            if j not in circularlist:
+                circularlist.append(j)
     item += 1
 print(circularlist)
+print(len(circularlist))
