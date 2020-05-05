bab50654bc8a639a4d56f17616897089a3e63a4d
ThomasR75 <ThomasR75@gmail.com>
2020-05-05 18:42:59 +0900

    Euler 46 roadmap

---
+++ Euler46.py
@@ -0,0 +1,8 @@
+#Goldbachs other conjecture
+#create list of odd numbers
+
+#create list of primes
+
+#take out all primes of odd number list
+
+#take away prime from odd number check if half residual is square, check until prime is larger the odd number
