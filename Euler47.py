cd4001fb72161b890648855a3befa5ded133d28d
ThomasR75 <ThomasR75@gmail.com>
2020-08-10 08:48:52 +0900

    Euler 47

--- Euler47.py
+++ Euler47.py
@@ -1,12 +1,29 @@
 #Euler47
 #consecutive numbers with same digit prime factors
-start1 = 99999
+from time import time
+#create list of primes
+primemax = 100000
+marked = [0] * primemax
+primes = [2, ]
+value = 3
+
+while value < primemax:
+    if marked[value] == 0:
+        primes.append(value)
+        i = value
+        while i < primemax:
+            marked[i] = 1
+            i += value
+    value += 2
+#parameters
+begin = time()
+start1 = 132000
 div = 2
 step = 0
 #find prime factors
 factors = []
 integers = []
-while start1 < 299999:
+while start1 < 135000:
     start = start1
     while div < start1:
         step = start / div
@@ -14,7 +31,10 @@ while start1 < 299999:
             factors.append(div)
             start = step
         else:
-            div += 1
+            if div > 2:
+                div += 2
+            else:
+                div += 1
     factorset = set(factors)
     if len (list (factorset)) == 4:
         integers.append(start1)
@@ -24,10 +44,12 @@ while start1 < 299999:
     factors = []
 count = len(integers)
 x = 0
-while x < count:
+while x < count-3:
     if integers[x] * 3 == integers[x+1] + integers[x+2] + integers[x+3] - 6:
             #if integers[x] == integers[x+3] - 3:
                 print (integers[x])
                 x += 1
     else:
         x += 1
+end = time()
+print("Time", end-begin)
