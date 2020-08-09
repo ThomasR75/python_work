a3df5c41e7866f493501b7090662e3c770e8987d
ThomasR75 <ThomasR75@gmail.com>
2020-08-09 23:00:50 +0900

    Euler 47

--- Euler47.py
+++ Euler47.py
@@ -1,12 +1,33 @@
 #Euler47
 #consecutive numbers with same digit prime factors
-
-start = 10
+start1 = 99999
 div = 2
 step = 0
 #find prime factors
 factors = []
-while div < math.sqrt(start):
-    step = start / div
-    if step.is_integer():
-        factors.append(div)
+integers = []
+while start1 < 299999:
+    start = start1
+    while div < start1:
+        step = start / div
+        if step.is_integer():
+            factors.append(div)
+            start = step
+        else:
+            div += 1
+    factorset = set(factors)
+    if len (list (factorset)) == 4:
+        integers.append(start1)
+
+    div = 2
+    start1 += 1
+    factors = []
+count = len(integers)
+x = 0
+while x < count:
+    if integers[x] * 3 == integers[x+1] + integers[x+2] + integers[x+3] - 6:
+            #if integers[x] == integers[x+3] - 3:
+                print (integers[x])
+                x += 1
+    else:
+        x += 1
